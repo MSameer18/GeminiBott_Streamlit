09fd5b7cc71c3fa2e1cc57fab74b463c970c091f
Muhammad Sameer <sameeriqbal452@gmail.com>
2025-12-22 23:35:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ st.set_page_config(
     layout="centered",  # Page layout option
 )
 
-GOOGLE_API_KEY = os.getenv("AIzaSyB6xW7woEQDit75alinMcFICjsv9eHEf_I")
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
-# Set up Google Gemini-Pro AI model
 gen_ai.configure(api_key=GOOGLE_API_KEY)
-model = gen_ai.GenerativeModel('gemini-pro')
+model = gen_ai.GenerativeModel("gemini-1.5-flash")
 
 
 # Function to translate roles between Gemini-Pro and Streamlit terminology
